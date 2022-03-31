e839b97312b2afd74d72b2580b74bad2a1fbb54a
joseluistl <66537882+joseluistl@users.noreply.github.com>
2022-03-31 01:40:18 -0600

    Update wordrelatedness.py

--- wordrelatedness.py
+++ wordrelatedness.py
@@ -1,4 +1,4 @@
-#! PATH/TO/ENVIRONMENT/BIN
+#! /home/joseluistl/anaconda3/bin/python
 
 import os
 import re
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from scipy import spatial
+from functools import reduce
 
 class WordRelate:
     """
@@ -20,6 +21,7 @@ class WordRelate:
     """
 
     def __init__(self, home_dir='.'):
+
         self.home_dir = home_dir
         self.data_path = os.path.join(self.home_dir, 'data', 'text_comp', 'texts')
         self.fig_path = os.path.join(self.home_dir, 'figs')
@@ -27,7 +29,7 @@ class WordRelate:
         # We'll store all our results at a collection based level i.e.
         # we are going to access every structure within the class
         # using the collection id as keys.
-        # TODO
+        #
         # Initialize the values (empty dics) for
         # - voc
         # - ivoc
@@ -38,6 +40,7 @@ class WordRelate:
         # -----------------------------------
 
         # Your code goes here (~ 1 - 5 lines)
+        self.content = {}
         self.voc = {}
         self.ivoc = {}
         self.collections = {}
@@ -46,7 +49,15 @@ class WordRelate:
         self.words = {}
 
         print(f"Files found in storage: \n {os.listdir(self.data_path)}")
+        collections_found = os.listdir(self.data_path)
+        for folder in collections_found:
+            self.collections[folder] = None
 
+    def get_collections(self):
+        return self.collections
+
+    def print_collections(self):
+        print(self.collections)
 
     @staticmethod
     def proc_line(line):
@@ -60,16 +71,14 @@ class WordRelate:
         :return: processed line
         """
         # -----------------------------------
-        # TODO
+        #
         # complete the parsers, currently, each word
         # line is output as it is.
         # (10 points)
         # -----------------------------------
 
-        #Esto ya debe recibir la línea en lower
+        # Esto ya debe recibir la línea en lower
         return re.sub(f"[{string.punctuation}]+", "", line).split()
-
-
 
 
     def get_voc(self, collection_id, sw, top_freq_words=2000):
@@ -85,7 +94,7 @@ class WordRelate:
             - self.ivoc: a series containing the vocabulary: {index: word} (this will be handy for word retrieval)
         """
         # -----------------------------------
-        # TODO
+        #
         # Parse the texts in the collection and
         # build a series with the top_freq_words.
         # Sort the index of voc based on the words.
@@ -94,24 +103,21 @@ class WordRelate:
         # -----------------------------------
 
         # Your code goes here (~ 2 lines)
+        # self.read_collection(collection_id)
+
+
         # Primero buscamos todas las palabras y contamos cuantas veces aparecen
         # Luego ordenamos por número de apraciones
         # Luego les damos el indice por orden de apraciones
         # Nos quedamos solo con las primeras 2000 palabras
 
-        self.read_collection(collection_id)
-    
-        
-        self.voc[collection_id] = pd.value_counts(self.content
-                            [collection_id]).sort_values().iloc[0:min
-                            (top_freq_words, len(self.words
-                            [collection_id]))].sort_index()
-        
-        self.ivoc[collection_id] = pd.Series(self.voc[collection_id].index, 
-                            index = self.voc[collection_id].values).sort_index()
-        
+        self.read_collection(collection_id, sw)
+        aux = pd.value_counts(self.content[collection_id]).sort_values().iloc[0:min(top_freq_words, len(self.words[collection_id]))].sort_index()
+        print(len(self.content[collection_id]))
+        words = np.unique(self.content[collection_id])
+        self.voc[collection_id] = pd.Series({aux.index[i]: i for i in range(len(aux))})
+        self.ivoc[collection_id] = pd.Series(self.voc[collection_id].index, index=self.voc[collection_id].values).sort_index()
         print(f'Monotonic index:{self.voc[collection_id].index.is_monotonic}')
-
 
     def dist_rep(self, collection_id, ws=4):
         """
@@ -126,23 +132,20 @@ class WordRelate:
         """
         voc_size = len(self.voc[collection_id])
         self.vrm[collection_id] = np.zeros((voc_size, voc_size))
-        
+
         for text in self.collections[collection_id]:
             indexes = []
-            
+
             for line in text:
-                line = np.array([w for w in line if w in self.voc[collection_id]], 
-                                dtype=object)
-                
-                for index in range(0, len(line)+1):
-                    aux = np.concatenate((line[max(0, index-ws):index], line[index+1, (min(len(line), index+ws)+1)]), axis=0)
-                    indexes.append(self.voc[line[index]], np.array([self.voc[x] for x in aux]))
-                    
-            indexes = np.array(indexes)
-            
-            for word in indexes:    
+                line = np.array([w for w in line if w in self.voc[collection_id]], dtype=object)
+                for index in range(0, len(line)):
+                    aux = np.concatenate((line[max(0, index - ws):index], line[index + 1: (min(len(line), index + ws) + 1)]), axis=0)
+                    indexes.append((self.voc[collection_id][line[index]], np.array([self.voc[collection_id][x] for x in aux])))
+            indexes = np.array(indexes, dtype=object)
+            for word in indexes:
                 for values in word[1]:
-                    self.vrm[word[0]][values] += 1
+                    self.vrm[collection_id][word[0]][values] += 1
+
 
     def ppmi_reweight(self, collection_id):
         """
@@ -157,11 +160,10 @@ class WordRelate:
         # -----------------------------------
 
         # Your code goes here (~ 1 - 4 lines)
-        total = sum(sum(self.voc[collection_id]))
+        total = sum(sum(self.vrm[collection_id]))
         sum_rows = np.array([sum(x) for x in self.vrm[collection_id]])
         sum_cols = sum(self.vrm[collection_id])
         expected = np.array([x*sum_cols for x in sum_rows])/total
-
         with np.errstate(divide='ignore'):
             log_vals = np.log(self.vrm[collection_id]/expected)
         self.vrm[collection_id] = np.maximum(log_vals, 0)
@@ -190,14 +192,15 @@ class WordRelate:
             os.mkdir(self.fig_path)
 
         for i, collection in enumerate(self.collections.keys()):
-            fig, ax = plt.subplots(figsize=(12, 10))
-            x = self.reduced_vrm[collection][:, 0]
-            y = self.reduced_vrm[collection][:, 1]
-            ax.scatter(x, y)
-            for i, txt in enumerate(self.ivoc[collection]):
-                ax.annotate(txt, (x[i], y[i]))
-            print(f'saving figure {collection}')
-            fig.savefig(os.path.join(self.fig_path, f'{collection}.png'))
+            if self.collections[collection] != None:
+                fig, ax = plt.subplots(figsize=(12, 10))
+                x = self.reduced_vrm[collection][:, 0]
+                y = self.reduced_vrm[collection][:, 1]
+                ax.scatter(x, y)
+                for i, txt in enumerate(self.voc[collection]):
+                    ax.annotate(txt, (x[i], y[i]))
+                print(f'saving figure {collection}')
+                fig.savefig(os.path.join(self.fig_path, f'{collection}.png'))
 
 
     def make_bulk_collections(self):
@@ -223,7 +226,7 @@ class WordRelate:
         for index, row in wr.iterrows():
             w1 = row.word1
             w2 = row.word2
-            s  = row.score
+            s = row.score
             # Check if words are in voc
             annotated_sim = []
             predicted_sim = []
@@ -239,12 +242,12 @@ class WordRelate:
         return correlation
 
     def read_collection(self, collection_id, sw):
-        collection_path = os.path.join(self.data_path, collection_id)
+        collection_path = f"{self.data_path}/{collection_id}"
         # Read each file in collection
         texts = []
         content = []
         for file in os.listdir(collection_path):
-            file_path = os.path.join(collection_path, file, 'main_text.txt')
+            file_path = os.path.join(collection_path, file)
             # Take care of gzipped files
             if re.match(r'^gzip', magic.from_file(file_path)):
                 with gzip.open(file_path, 'rb') as fa:
@@ -262,37 +265,37 @@ class WordRelate:
             # -----------------------------------
 
             # Your code goes here (~ 1 - 3 lines)
-            to_delete= '\n'
-            lines = [self.proc_line(f"START {re.sub(r'to_delete', '', x).lower()} END") 
-                     for x in lines if len(self.proc_line(x)) > 2]
-            content += [x for text in texts for x in text]                                                  
-            texts.append(np.array(lines))                                                  
-            
-        # Add texts to the collections.
+            to_delete = '\n'
+            lines = [self.proc_line(f"START {re.sub(r'to_delete', '', x).lower()} END") for x in lines if len(self.proc_line(x)) > 2]
+            content += [x for line in lines for x in line if x not in sw]
+            texts.append(np.array(lines, dtype=object))
+
+            # Add texts to the collections.
         # Texts es una lista que tiene listas (Cada una corresponde a un texto) y cada lista tiene listas con las
         # palabras de cada línea
+        self.content[collection_id] = np.array(content)
         self.words[collection_id] = np.unique(content)
-        self.collections[collection_id] = np.array(texts)
+        self.collections[collection_id] = np.array(texts, dtype=object)
 
 
 if __name__ == '__main__':
     # Check available collections
+    # Esto ya funciona
     collections = os.listdir(os.path.join('data', 'text_comp', 'texts'))
+    ##print(collections)
     # Parse arguments
     parser = ArgumentParser()
-    parser.add_argument('--collection',
-                        choices=collections)
+    parser.add_argument('--collection', choices=collections)
     args = parser.parse_args()
     # Read stopwords
-    with open('./data/stop_words/stopwords.txt') as f:
+    with open('./stopwords.txt') as f:
         stopwords = f.read().split('\n')
     # Instantiate WordRelate object
     wc = WordRelate()
     # Read collection argument
     collection = args.collection
-
     # -----------------------------------
-    # TO DO
+    # TODO
     # -----------------------------------
     # 1.- read collection 00
     # 2.- get vocabularies
@@ -308,9 +311,14 @@ if __name__ == '__main__':
     # -----------------------------------
 
     # Your code goes here (~ 7 lines)
-    wc.read_collection(1, stopwords)
-    wc.get_voc(collection_id=1, sw=stopwords)
-    wc.dist_rep(collection_id=1)
-    wc.ppmi_reweight(collection_id=1)
-    wc.dim_redux(collection_id=1)
+
+    # Read collection 130
+    wc.read_collection(73, stopwords)
+    wc.get_voc(collection_id = 73, sw = stopwords)
+    wc.dist_rep(collection_id = 73)
+    wc.ppmi_reweight(collection_id = 73)
+    wc.dim_redux(collection_id = 73)
     wc.plot_reps()
+
+
+   
